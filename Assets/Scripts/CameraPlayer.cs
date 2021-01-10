4daca9ea8c3460ae0ee4ef46b1ec1582b4802d3e
SWoolman <sandy@woolmans.net>
2021-01-10 17:46:14 +0100

    added more assets

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+   public Transform playerCharacter;
+   public float cameraDistance = 30.0f;
+   public Transform left;
+   public Transform right;
+   public Transform bottom;
+   public Transform top;
+
+   private void Awake()
+   {
+      GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+   }
+
+   private void FixedUpdate()
+   {
+      transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, transform.position.z);
+      //Screen.width / 2, Screen.height / 2;
+      transform.position = Clamp(transform.position, 
+         new Vector3(left.position.x, bottom.position.y, transform.position.z),
+         new Vector3(right.position.x, top.position.y, transform.position.z));
+   }
+
+   private Vector3 Clamp(Vector3 value, Vector3 min, Vector3 max)
+   {
+      return new Vector3
+      {
+         x = Mathf.Clamp(value.x, min.x, max.x),
+         y = Mathf.Clamp(value.y, min.y, max.y),
+         z = Mathf.Clamp(value.z, min.z, max.z)
+      };
+   }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    // Handle the enemy patrol
+
+    public float speed;
+    public float distance;
+    
+    private bool movingLeft_ = true;
+    
+    public Transform groundDetection;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+            if (movingLeft_ == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft_ = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0,0,0);
+                movingLeft_ = true;
+            }
+        {
+            
+        }
+    }
+}
