dd6239a8186f78681573e7d7dbed7f8bdcee3451
SWoolman <sandy@woolmans.net>
2021-01-12 15:40:03 +0100

    fixed menu controls

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuScript : MonoBehaviour
 {
+   
+   public GameObject menuPlayButton, menuCommandButton, menuExitButton, menuReturnButton;
    public void PlayTheGame()
    {
       SceneManager.LoadScene("Game_Scene");
+      
+      EventSystem.current.SetSelectedGameObject(null);
+        
+      EventSystem.current.SetSelectedGameObject(menuPlayButton);
+   }
+
+   public void CommandMenu()
+   {
+      SceneManager.LoadScene("Command_Menu");
+      
+      EventSystem.current.SetSelectedGameObject(null);
+        
+      EventSystem.current.SetSelectedGameObject(menuCommandButton);
+   }
+   
+   public void ReturnMenu()
+   {
+      SceneManager.LoadScene("Main_Menu");
+
+      EventSystem.current.SetSelectedGameObject(null);
+        
+      EventSystem.current.SetSelectedGameObject(menuReturnButton);
    }
 
    public void QuitTheGame()
    {
       Debug.Log("QUIT THE GAME");
       Application.Quit();
+      
+      EventSystem.current.SetSelectedGameObject(null);
+        
+      EventSystem.current.SetSelectedGameObject(menuExitButton);
    }
 }
